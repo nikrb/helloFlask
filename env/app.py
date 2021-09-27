e59fcecdbf39d1dd907842a838c24b6ea7242c9f
nikrb <nik_rb@yahoo.com>
2021-09-27 22:58:51 +0100

    initial commit

---
+++ env/app.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from flask import Flask
+from flask import render_template
+import re
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, Flask!"
+
+@app.route("/hello/<name>")
+def helloThere(name = "none"):
+    return render_template(
+        "hello_there.html",
+        name = name,
+        date = datetime.now()
+    )
+
+@app.route("/api/data")
+def get_data():
+    return app.send_static_file("data.json")
